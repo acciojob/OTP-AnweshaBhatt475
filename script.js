2d685a005a64f5dacd3e90ff4153706fdbf35700
AnweshaBhatt475 <143898640+AnweshaBhatt475@users.noreply.github.com>
2025-02-14 23:40:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your JS code here. If required.
+/*  your css code here. If applicable */
+
+    
+    const codes = document.querySelectorAll('.code');
+    
+    for (let i = 0; i < codes.length; i++) {
+        codes[i].addEventListener('input', function () {
+            // If a digit is entered and not the last field, move to the next field
+            if (codes[i].value.length === 1 && i < codes.length - 1) {
+                codes[i + 1].focus();
+            }
+        });
+
+        codes[i].addEventListener('keydown', function (e) {
+            // If backspace is pressed and not the first field, move to the previous field
+            if (e.key === 'Backspace' && i > 0 && codes[i].value.length === 0) {
+                codes[i - 1].focus();
+            }
+        });
+    }
+
